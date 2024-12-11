6016e1b728a8e83467e009e8ffb3a340d278be36
mangesh242 <mangeshshinde242@gmail.com>
2024-12-11 22:20:12 +0530

    first commit

---
+++ src/main/java/org/da1/productcatalog/models/BaseModel.java
@@ -0,0 +1,10 @@
+package org.da1.productcatalog.models;
+import java.util.Date;
+
+
+public class BaseModel {
+    private long id;
+    private Date createdAt;
+    private Date updatedAt;
+    private State state;
+}
---
+++ src/main/java/org/da1/productcatalog/models/State.java
@@ -0,0 +1,6 @@
+package org.da1.productcatalog.models;
+
+public enum State {
+    ACTIVE,
+    INACTIVE
+}
