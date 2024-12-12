10426956c3656e361234a9093f44338cf401a517
mangesh242 <mangeshshinde242@gmail.com>
2024-12-12 16:58:11 +0530

    resolved conflict

--- src/main/java/org/da1/productcatalog/controllers/CategoryController.java
+++ src/main/java/org/da1/productcatalog/controllers/CategoryController.java
@@ -2,8 +2,11 @@ package org.da1.productcatalog.controllers;
 
 import org.da1.productcatalog.models.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -11,7 +14,24 @@ import java.util.List;
 public class CategoryController {
 
 
-    public List<Category> getCategory(long id){
-        return null;
+
+    @GetMapping("/category")
+    public List<Category> getAllCategories(){
+        List<Category> categories = new ArrayList<>();
+        Category category = new Category();
+        category.setId(2L);
+        category.setName("house");
+        categories.add(category);
+        return categories;
     }
+
+    @GetMapping("/category/{id}")
+    public Category getCategory(@PathVariable Long id){
+
+        Category category = new Category();
+        category.setId(id);
+        return category;
+
+    }
+
 }
